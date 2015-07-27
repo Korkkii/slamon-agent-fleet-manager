4a3d865de4d81a9449c3e406e3a2b1ef2c31fdf7
Henri Korhonen <henri.korhonen@f-secure.com>
2015-07-27 10:36:50 +0300

    Deleted automated deployment.

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import find_packages, setup
-# TODO: Fix for AFM
 
-short_description = 'SLAMon Agent Fleet Manager is part of the Coordinator, which controls deployed agents by giving them tasks and receiving the results. The results are then propagated to e.g. a business process management system (BPMS).'
-try:
-    import pypandoc
-    description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    description = short_description
+short_description = 'SLAMon Agent Fleet Manager is part of the Coordinator, which controls deployed agents by giving ' \
+                    'them tasks and receiving the results. The results are then propagated to e.g. a business process ' \
+                    'management system (BPMS).'
 
 setup(
     name='slamon-afm',
@@ -19,7 +15,6 @@ setup(
     author_email='slamon.organization@gmail.com',
     license='Apache License v2.0',
     platforms=['Python 3.3+'],
-    long_description=description,
     packages=find_packages(),
     install_requires=[
         'bottle>=0.12.8, <1.0',
@@ -27,12 +22,6 @@ setup(
         'jsonschema>=2.5.1, <3.0',
         'python_dateutil>= 2.4.2, <3.0'
     ],
-    entry_points={
-        # TODO: Create AFM console script
-        'console_scripts': [
-            'slamon-agent = slamon_agent.agent:main'
-        ]
-    },
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
