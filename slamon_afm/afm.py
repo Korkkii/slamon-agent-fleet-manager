bb9cb7e0f64bef89ba1d9364ee264e59714a7ea2
Henri Korhonen <henri.korhonen@f-secure.com>
2015-07-29 11:02:54 +0300

    Fixing commandline script

--- slamon_afm/afm.py
+++ slamon_afm/afm.py
@@ -37,6 +37,7 @@ LOGGING = {
     }
 }
 
+
 def main():
     logging.basicConfig(level=logging.INFO)
 
@@ -44,20 +45,38 @@ def main():
         pass
 
     parser = argparse.ArgumentParser(description='Admin util for SLAMon Agent Fleet Manager')
-    parser.add_argument('-c', '--create-tables', help='Create tables', action='store_true', default=False)
-    parser.add_argument('-d', '--drop-tables', help='Drop all tables', action='store_true', default=False)
-    parser.add_argument('-n', '--host', help='Host name', action='store', required=True)
+    subparsers = parser.add_subparsers()
+
+    run_parser = subparsers.add_parser('run', help='Run AFM',
+                                       description='Run an instance of an Agent Fleet Manager that listens to given host address')
+    run_parser.add_argument('HOST', help='Host name or address e.g. localhost or 127.0.0.1', action='store',
+                            required=True)
+    run_parser.set_defaults(func=run_afm)
+
+    create_parser = subparsers.add_parser('create-tables', help='Create SQL tables',
+                                          description='Create required database tables to PostgreSQL')
+    create_parser.set_defaults(func=create)
+
+    drop_parser = subparsers.add_parser('drop-tables', help='Drop SQL tables',
+                                        description='Drop created database tables from PostgreSQL')
+    drop_parser.set_defaults(func=drop)
 
     init_connection(unittest=False)
 
     args = parser.parse_args()
-    if args.create_tables is True:
-        create_tables()
-    elif args.drop_tables is True:
-        drop_tables()
+    args.func(args)
 
+
+def run_afm(args):
     run(afm_app.app, host=args.host, port=Settings.port, debug=True)
 
+
+def create(args):
+    create_tables()
+
+
+def drop(args):
+    drop_tables()
 
 if __name__ == '__main__':
     main()
